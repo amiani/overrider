db9b2cfa19ff08644bcb5b8746602f25656a4bdd
Amiani Johns <amiani.johns@ziffmedia.com>
2021-12-03 17:57:13 -0500

    calculate and report MAD instead of std

--- src/summarize.js
+++ src/summarize.js
@@ -1,13 +1,21 @@
-const mean = (arr = []) => arr.reduce((a, b) => a + b) / arr.length;
-const median = (arr = []) => {
-	const sorted = arr.sort((a, b) => a - b);
+const mean = (nums = []) => nums.reduce((a, b) => a + b) / nums.length;
+
+const median = (nums = []) => {
+	const sorted = nums.sort((a, b) => a - b);
 	const mid = Math.floor(sorted.length / 2);
 	return sorted.length % 2 === 0 ? (sorted[mid - 1] + sorted[mid]) / 2 : sorted[mid];
 };
-const std = (arr = []) => {
-	const avg = mean(arr);
-	const variance = arr.reduce((a, b) => a + Math.pow(b - avg, 2), 0) / (arr.length - 1);
-	return Math.sqrt(variance);
+
+const variance = (nums = []) => {
+	const avg = mean(nums);
+	return nums.reduce((a, b) => a + Math.pow(b - avg, 2), 0) / (nums.length - 1);
+};
+
+const std = (nums = []) => Math.sqrt(variance(nums));
+
+const mad = (nums = []) => {
+	const med = median(nums);
+	return median(nums.map(num => Math.abs(num - med)));
 };
 
 export const summarize = (nums = []) => ({
@@ -16,11 +24,12 @@ export const summarize = (nums = []) => ({
 	max: Math.max(...nums),
 	mean: mean(nums),
 	median: median(nums),
+	mad: mad(nums),
 	std: std(nums),
 });
 
 export const printSummary = (scale = 2) => (summary) => `\
 min: ${summary.min.toFixed(scale)} mean: ${summary.mean.toFixed(scale)} \
-median: ${summary.median.toFixed(scale)} std: ${summary.std.toFixed(scale)} \
+median: ${summary.median.toFixed(scale)} MAD: ${summary.mad.toFixed(scale)} \
 max: ${summary.max.toFixed(scale)}\
 `;
